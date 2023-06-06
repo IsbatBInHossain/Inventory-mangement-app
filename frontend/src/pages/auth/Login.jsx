a0bcef80ad34f86ec2676f19b9e084d87672ea11
Isbat-Bin-Hossain <isbat31416@gmail.com>
2023-06-06 15:32:51 +0600

    Add auth routes, Create Card compoonent, Add Login Page

---
+++ frontend/src/pages/auth/Login.jsx
@@ -0,0 +1,38 @@
+import styles from './auth.module.scss';
+import { BiLogIn } from 'react-icons/bi';
+import { Link } from 'react-router-dom';
+import Card from '../../components/card/card';
+
+const Login = () => {
+  return (
+    <div className={`container ${styles.auth}`}>
+      <Card>
+        <div className={styles.form}>
+          <div className='--flex-center'>
+            <BiLogIn size={35} color='#999' />
+          </div>
+          <h2>Login</h2>
+          <form>
+            <input type='text' name='email' required placeholder='Email' />
+            <input
+              type='text'
+              name='password'
+              required
+              placeholder='Password'
+            />
+            <button className='--btn --btn-primary --btn-block' type='submit'>
+              Login
+            </button>
+          </form>
+          <Link to='/forgotpassword'>Forgot Password</Link>
+          <span className={styles.register}>
+            <Link to='/'>Home</Link>
+            <p>&nbsp; Don{`'`}t Have an account? &nbsp;</p>
+            <Link to='/register'>Register</Link>
+          </span>
+        </div>
+      </Card>
+    </div>
+  );
+};
+export default Login;
